6590d6fb98c4dead491eb40a864d69d704f95ec2
Vivekanand Pujar <vivek@vivekanand.dev>
2026-08-17 15:37:24 +0530

    api implementation

--- src/main/java/dev/vivekanand/aqrspringbootworkshop/apis/CustomerApi.java
+++ src/main/java/dev/vivekanand/aqrspringbootworkshop/apis/CustomerApi.java
@@ -1,10 +1,44 @@
 package dev.vivekanand.aqrspringbootworkshop.apis;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.vivekanand.aqrspringbootworkshop.entities.Customer;
+import dev.vivekanand.aqrspringbootworkshop.services.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerApi {
+    private final CustomerService customerService;
 
+    public CustomerApi(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getCustomers());
+    }
+
+    @GetMapping("{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") Long customerId) {
+        return ResponseEntity.ok(customerService.getCustomer(customerId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.createCustomer(customer));
+    }
+
+    @PutMapping("{customerId}")
+    public ResponseEntity<Customer> updateCustomer( @PathVariable long customerId, @RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.updateCustomer(customerId, customer));
+    }
+
+    @DeleteMapping("{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable long customerId) {
+        customerService.deleteCustomer(customerId);
+        return ResponseEntity.noContent().build();
+    }
 }
