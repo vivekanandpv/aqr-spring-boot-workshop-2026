1f21df9edeeb9426d1f2e96c970a5b4c8fb4599d
Vivekanand Pujar <vivek@vivekanand.dev>
2026-08-17 15:58:12 +0530

    Spel configuration demo 1

--- src/main/java/dev/vivekanand/aqrspringbootworkshop/apis/SampleApi.java
+++ src/main/java/dev/vivekanand/aqrspringbootworkshop/apis/SampleApi.java
@@ -3,6 +3,7 @@ package dev.vivekanand.aqrspringbootworkshop.apis;
 import dev.vivekanand.aqrspringbootworkshop.entities.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,16 +14,20 @@ import java.util.Map;
 public class SampleApi {
     private static final Logger LOGGER = LoggerFactory.getLogger(SampleApi.class);
 
+    private final String key;
+
+    public SampleApi(
+            @Value("${app.sample.key}") String key
+    ) {
+        this.key = key;
+    }
+
     @GetMapping
     public ResponseEntity<Map<String, String>> sample(
             @RequestParam(required = false, defaultValue = "London") String location,
             @RequestParam(required = false, defaultValue = "default@gmail.com") String email
     ) {
-        if (true) {
-            throw new IllegalArgumentException("Oops!!");
-        }
-
-        return ResponseEntity.ok(Map.of("message", "Hello, world!", "location", location, "email", email));
+        return ResponseEntity.ok(Map.of("message", "Hello, world!", "location", location, "email", email, "key", key));
     }
 
     @PostMapping
